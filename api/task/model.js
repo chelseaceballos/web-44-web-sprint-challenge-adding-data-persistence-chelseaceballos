0f888327e2be6b62c65a4f50110bccc1a29a340d
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-13 16:52:54 -0700

    committing due to time, rather get whatever progress i have in even if it's not perfect

--- api/task/model.js
+++ api/task/model.js
@@ -5,6 +5,25 @@ function getAll() {
     return db('tasks')
 }
 
+function create(task) {
+    return db('tasks').insert(task).then(task_id => {
+        return db('tasks').where({ task_id }).first().then(
+            thing => {
+                return {
+                    ...thing,
+                    task_completed: thing.task_completed ? true === 0 : false
+                }
+            }
+        )
+    })
+};
+
+function getById(project_id) {
+    return db('projects').where({ project_id }).first()
+};
+
 module.exports = {
     getAll,
+    create,
+    getById,
 }
--- api/task/router.js
+++ api/task/router.js
@@ -3,6 +3,8 @@ const router = require('express').Router()
 const Task = require('./model')
 
 
+
+
 const idValidation = (req,res, next)=>{
     
     Task.getById(req.body.project_id)
@@ -17,9 +19,13 @@ const idValidation = (req,res, next)=>{
 };
 
 //  [GET] /api/tasks
-// Even though task_completed is stored as an integer, the API uses booleans when interacting with the client
-// Each task must include project_name and project_description
-// Example of response body: [{"task_id":1,"task_description":"baz","task_notes":null,"task_completed":false,"project_name:"bar","project_description":null}]
+// {
+//     "project_id": 1,
+//     "task_completed": 0,
+//     "task_description": "task desc 2",
+//     "task_id": 2,
+//     "task_notes": "task notes"
+// },
 
 router.get('/',  (req, res, next) => {
    Task.getAll()
@@ -30,11 +36,16 @@ router.get('/',  (req, res, next) => {
 });
 
 // [POST] /api/tasks
-// Even though task_completed is stored as an integer, the API uses booleans when interacting with the client
+// Even though task_completed is stored as an integer, the API uses booleans when interacting with the client :(
 // Example of response body: {"task_id":1,"task_description":"baz","task_notes":null,"task_completed":false,"project_id:1}
 router.post('/', idValidation, async (req, res, next) => {
-    res.json('posts a task')
+    Task.create(req.body)
+        .then(data => {
+            res.status(201).json(data)
+        })
+        .then(next)
 })
+
 
 router.use((err, req, res, next) => {
     res.status(500).json({
@@ -46,13 +57,3 @@ router.use((err, req, res, next) => {
 
 module.exports = router
 
-// [GET] /api/tasks
-// ✕ [10] can get all tasks in the table (17 ms)
-// ✕ [11] each task contains task_notes and task_description and task_completed (as a boolean) (14 ms)
-// ✕ [12] each task contains the project_name and the project_description (15 ms)
-//       [POST] /api/tasks
-// ✕ [13] can add a new task to the db (20 ms)
-// ✕ [14] responds with the newly created task with the task_completed as a boolean (13 ms)
-// ✕ [15] rejects a task lacking a task_description with an error status code (18 ms)
-// ✕ [16] rejects a task lacking a project_id with an error status code (15 ms)
-// ✕ [17] rejects a task containing an invalid project_id with an error status code (15 ms)
