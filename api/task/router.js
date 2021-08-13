2cda57d3a6678fd52439d13a1ed542cee08e951f
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-13 16:40:56 -0700

    idvalidation middleware

--- api/task/router.js
+++ api/task/router.js
@@ -1,6 +1,21 @@
 // build your `/api/tasks` router here
 const router = require('express').Router()
 const Task = require('./model')
+
+
+const idValidation = (req,res, next)=>{
+    
+    Task.getById(req.body.project_id)
+    .then(existing => {
+        if (!existing) {
+            next({ status: 400, message: 'id not found'})
+        } else {
+            next()
+        }
+    })
+    .catch(next)
+};
+
 //  [GET] /api/tasks
 // Even though task_completed is stored as an integer, the API uses booleans when interacting with the client
 // Each task must include project_name and project_description
@@ -17,7 +32,7 @@ router.get('/',  (req, res, next) => {
 // [POST] /api/tasks
 // Even though task_completed is stored as an integer, the API uses booleans when interacting with the client
 // Example of response body: {"task_id":1,"task_description":"baz","task_notes":null,"task_completed":false,"project_id:1}
-router.post('/', async (req, res, next) => {
+router.post('/', idValidation, async (req, res, next) => {
     res.json('posts a task')
 })
 
@@ -30,3 +45,14 @@ router.use((err, req, res, next) => {
 })
 
 module.exports = router
+
+// [GET] /api/tasks
+// ✕ [10] can get all tasks in the table (17 ms)
+// ✕ [11] each task contains task_notes and task_description and task_completed (as a boolean) (14 ms)
+// ✕ [12] each task contains the project_name and the project_description (15 ms)
+//       [POST] /api/tasks
+// ✕ [13] can add a new task to the db (20 ms)
+// ✕ [14] responds with the newly created task with the task_completed as a boolean (13 ms)
+// ✕ [15] rejects a task lacking a task_description with an error status code (18 ms)
+// ✕ [16] rejects a task lacking a project_id with an error status code (15 ms)
+// ✕ [17] rejects a task containing an invalid project_id with an error status code (15 ms)
