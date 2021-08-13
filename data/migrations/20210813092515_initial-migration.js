299e1afca7a61aed0ccc0db085a9b40fb4373bb3
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-13 10:22:46 -0700

    ran migrate:latest

--- data/migrations/20210813092515_initial-migration.js
+++ data/migrations/20210813092515_initial-migration.js
@@ -3,21 +3,36 @@ exports.up = async function(knex) {
   await knex.schema
     .createTable('projects', table => {
         table.increments('project_id')
+        table.string('project_name', 160).notNullable()
+        table.string('project_description')
+        table.boolean('project_completed').defaultTo(0) 
     })
     .createTable('resources', table => {
         table.increments('resource_id')
+        table.string('resource_name', 500).notNullable().unique()
+        table.string('resource_description')
     })
     .createTable('tasks', table => {
         table.increments('task_id')
+        table.string('task_description').notNullable()
+        table.string('task_notes')
+        table.boolean('task_completed').defaultTo(0)
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('RESTRICT') // or 'CASCADE' if you'd want to be able to delete or update
+            .onUpdate('RESTRICT')
     })
-    .createTable('project_resources', table => {
-        table.increments('project_resources_id')
-    })
+    // .createTable('project_resources', table => {
+    //     table.increments()
+    // })
 };
 
 exports.down = async function(knex) {
   await knex.schema
-    .dropTableIfExists('project_resource')
+    .dropTableIfExists('project_resources')
     .dropTableIfExists('tasks')
     .dropTableIfExists('resources')
     .dropTableIfExists('projects')
